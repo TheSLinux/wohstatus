8b6734f717b50c830f8a04c2ccb598b0fb3937c8
Ky-Anh Huynh <kyanh@theslinux.org>
2015-06-21 10:50:39 +0700

    update.sh: return status in api directory

--- update.sh
+++ update.sh
@@ -2,6 +2,10 @@
 
 ./check.sh
 ./compile.sh
+
+mkdir -pv output/api/
+./api.sh /status > ./output/api/status.yaml
+
 rsync -rapv --delete ./output/ $HOME/public_html/wohstatus/
 cd $HOME/public_html/wohstatus/
 ln -s status.html index.html
